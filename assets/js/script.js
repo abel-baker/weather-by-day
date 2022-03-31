35e7685b3ad386104bb8d011fa58b3ace2822c2d
thevcr <thevcr@gmail.com>
2022-03-31 18:36:10 -0400

    skeleton api call for address weather info

---
+++ assets/js/script.js
@@ -0,0 +1,50 @@
+var dateInputEl = document.querySelector("#date-input");
+var addressInputEl = document.querySelector("#address-input");
+var locationPresetEl = document.querySelector("#location-options");
+var locationSelectContainer = document.querySelector("#main-container");
+var weatherContainerEl = document.querySelector("#weather-container");
+var address;
+
+var locationSubmitHandler = function(event) {
+    // prevent page from refreshing
+    event.preventDefault();
+  
+    // get value from input element
+    address = addressInputEl.value.trim();
+  
+    if (address) {
+      getAddressWeatherData(address);
+  
+      // clear old content
+      weatherContainerEl.textContent = "";
+    } else {
+      alert("Please enter a valid date");
+    }
+};
+
+var getAddressWeatherData = function(address) {
+    // format the oikos api url
+    var apiUrl = "https://api.oikolab.com/weather/?api-key=deee70cd14c24615bca5e1bdf08ead2c&location=" + address;
+  
+    // make a get request to url
+    fetch(apiUrl)
+      .then(function(response) {
+        // request was successful
+        if (response.ok) {
+          console.log(response);
+          response.json().then(function(data) {
+            console.log(data);
+            // display weather data function here
+          });
+        } else {
+          alert("Error: " + response.statusText);
+        }
+      })
+      .catch(function(error) {
+        alert("Unable to connect to Oikolab");
+      });
+  };
+
+
+  locationSelectContainer.addEventListener("submit", locationSubmitHandler);
+  
