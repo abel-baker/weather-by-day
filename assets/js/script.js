be5aca51f7e2f9cdf5eef924dacf1dc531474d40
Hanna L <hannalee92@gmail.com>
2022-04-05 19:46:19 -0400

    initial dom element upload

---
+++ assets/js/script.js
@@ -0,0 +1,51 @@
+const whenAndWhereEl = document.querySelector("#when-and-where");
+const locationH2 = document.createElement("h2")
+whenAndWhereEl.appendChild(locationH2)
+
+
+
+
+const locationSection = document.createElement("section")
+locationSection.setAttribute("class", "box")
+
+locationSection.setAttribute("id", "when-and-where")
+
+locationH2.setAttribute("class", "is-size-4")
+
+locationSection.appendChild(locationH2)
+locationH2.textContent = "placeholder"
+
+
+
+
+
+
+const weatherFeaturesEl = document.querySelector("#weather-features");
+const locationH2 = document.createElement("h2")
+weatherFeaturesEl.appendChild(locationH2)
+
+
+
+
+const weatherFeaturesSection = document.createElement("section")    
+weatherFeaturesSection.setAttribute("class", "")
+
+weatherFeaturesSection.setAttribute("id", "weather-features")
+
+const weatherFeaturesArticle = document.createElement("article")
+weatherFeaturesArticle.setAttribute("class", "media px-4")
+
+const weatherFeaturesFigure = document.createElement("figure")
+weatherFeaturesFigure.setAttribute("class", "media-left")
+
+const weatherFeaturesP = document.createElement("p")
+weatherFeaturesP.setAttribute("class", "icon")
+
+i
+
+const weatherFeaturesDiv = document.createElement("div")
+weatherFeaturesDiv.setAttribute("class", "media-content")
+
+
+
+
